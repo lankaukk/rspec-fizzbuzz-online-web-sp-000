629a727b8e0383cc911d823ec113e48bf8246f83
McKayla Lankau <lankaukk@yahoo.com>
2020-07-16 20:13:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,10 @@
 def fizzbuzz(int)
   if int % 5 == 0 and int % 3 == 0
     "Fizzbuzz"
-  
-    
   elsif int % 5 == 0 
     "Buzz"
-  
-    
   elsif int % 3 == 0 
     "Fizz"
-  
-    
   else 
     "nil"
   end
